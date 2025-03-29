b70354f4919d5e49c786619c46d15ef36a041963
Пользователь <ablaze.coder@proton.me>
2025-03-29 07:02:09 +0500

    Added redirect from home page to admin panel

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    # Bosh sahifani admin paneliga yo'naltirish
+    path('', RedirectView.as_view(url='/admin/', permanent=True)),
 ]
 
 if settings.DEBUG:
