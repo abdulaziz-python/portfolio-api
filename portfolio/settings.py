fb095c7fe4c45642911fdc84b9af41de3021be5d
Пользователь <ablaze.coder@proton.me>
2025-03-29 06:51:30 +0500

    PostgreSQL configuration added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from django.urls import reverse_lazy
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,11 +84,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# PostgreSQL ma'lumotlar bazasiga ulanish
+DATABASE_URL = "postgres://avnadmin:AVNS_dPjmnTXQbvMRCANMDZx@portfolio-database-ablaze-coder.k.aivencloud.com:18321/defaultdb?sslmode=require"
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
 }
 
 
